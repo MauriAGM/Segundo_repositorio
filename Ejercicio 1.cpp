44f83b505b3917b33eb3417154c2bf263b88f5f9
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-05-28 21:18:59 -0500

    Actualizacion ejercicio 1: agregar validaciones

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -3,10 +3,20 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Ingrese la cantidad de multiplos de 7: ";
-    cin >> n;
+
+    do {
+        cout << "Ingrese la cantidad de multiplos de 7 (mayor que 0): ";
+        cin >> n;
+        if (n <= 0) {
+            cout << "Error: Debe ingresar un numero positivo mayor que cero.\n";
+        }
+    } while (n <= 0);
+
+    cout << "\nLos primeros " << n << " multiplos de 7 son:\n";
     for (int i = 1; i <= n; i++) {
         cout << i * 7 << endl;
     }
+
     return 0;
 }
+
