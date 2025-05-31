1aabb11ee1c411958d8e128c73cbf8c59f3f9ca6
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-05-30 22:35:33 -0500

    Actualizacion ejercicio 10: mejora y agregar validaciones

--- Ejercicio 10.cpp
+++ Ejercicio 10.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int num, cont = 0;
-    cout << "Ingrese numero: ";
-    cin >> num;
-    for (int i = 1; i <= num; i++) {
-        if (num % i == 0) cont++;
-    }
-    if (cont == 2) cout << "Es primo.\n";
-    else cout << "No es primo.\n";
-    return 0;
+int main(){
+	int num; 
+	cout << "Ingrese un numero entero positivo mayor que 1: ";
+	cin >> num;
+	if ( num <= 1) {
+		cout << "El numero debe ser mayor que 1. \n";
+		return 1;
+	}
+	
+	bool esPrimo = true;
+	for (int i = 2; i * i <= num; i++) {
+		if (num % i == 0) {
+			esPrimo = false;
+			break;
+		}
+	}
+	
+	if (esPrimo)
+		cout << "Es primo.\n";
+	else 
+		cout << "No es primo.\n";
+	
+	return 0;
 }
