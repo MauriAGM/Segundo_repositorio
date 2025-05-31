dfc6be759baaeb70a20ee1ef38cc1b454261708f
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-05-30 22:36:12 -0500

    Actualizacion del ejercicio 11: agregar validaciones

--- Ejercicio 11.cpp
+++ Ejercicio 11.cpp
@@ -4,8 +4,12 @@ using namespace std;
 int main() {
     int n;
     double factorial = 1;
-    cout << "Numero: ";
+    cout << "Ingrese un numero entero no negativo: ";
     cin >> n;
+    if (n < 0){
+    	cout << "Ingrese el factorial no esta definido para numeros negativos.\n";
+    	return 1;
+	}
     for (int i = 1; i <= n; i++) {
         factorial *= i;
     }
