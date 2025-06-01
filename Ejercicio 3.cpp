1164b7c4876838af2d23cf09500cba1e36697cc3
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-05-31 23:45:39 -0500

    Actualizacion ejercicio 3: agregar validaciones

--- Ejercicio 3.cpp
+++ Ejercicio 3.cpp
@@ -5,9 +5,13 @@ int main() {
     int n, suma = 0;
     cout << "Ingrese un numero: ";
     cin >> n;
-    for (int i = 1; i <= n; i++) {
+    if (n > 0){
+    	for (int i = 1; i <= n; i++) {
         suma += i;
-    }
-    cout << "Suma = " << suma << endl;
+		}
+		cout << "Suma = " << suma << endl;
+    } else {
+    	cout << "Numero invalido." << endl;
+	}
     return 0;
 }
